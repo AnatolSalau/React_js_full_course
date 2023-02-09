a5157ac447735bd84182d2dc7afdbe6deb72f4c0
AnatolSalau <salovanatoly5@mail.ru>
2023-02-09 12:18:18 +0300

    lesson_3_html_04_react_green_button - worked version

--- lesson_3_html_react_green_button/script.js
+++ lesson_3_html_react_green_button/script.js
@@ -1,15 +1,28 @@
 console.log("hello from script src")
 
-let buttonText = 'Click me';
 //Create element by JSX
-const helloWorldJsxHeading = (
-    <div className = "default-btn">
-        <button onClick = {()=> (buttonText = 'Hello from react')}>
-            {buttonText}
-        </button>
-    </div>
-);
+const App = () => {
+    /**
+     * Hook useState
+     * 1. destructuring object
+     * initial value is buttonText = 'Click me' (buttonText = 'Click me')
+     * !!! setButtonText - it is function -> we must call a function
+     */
+    let [buttonText, setButtonText] = React.useState('Click me');
+    
+    const onButtonClick = ()=> {
+        setButtonText('Hello from react');
+    };
+    
+    return (
+        <div className = "default-btn">
+            <button onClick = {onButtonClick}>
+                {buttonText}
+            </button>
+        </div>
+    );
+}
 
 const containerJSX = document.getElementById('appJsx');
 const  rootJsx = ReactDOM.createRoot(containerJSX);
-rootJsx.render(helloWorldJsxHeading);
+rootJsx.render(<App />);
