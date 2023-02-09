991ac2d2c6d2bb9091628d919b44db67a25f0275
AnatolSalau <salovanatoly5@mail.ru>
2023-02-09 13:04:10 +0300

    lesson_3_html_04_react_green_button - worked version

--- lesson_3_html_react_green_button/script.js
+++ lesson_3_html_react_green_button/script.js
@@ -1,15 +1,22 @@
 console.log("hello from script src")
 
-//Create functional component
-const App = () => {
+/**
+ * Destructuring an object into its properties
+ * @param props - default text for button
+ * @returns {JSX.Element}
+ */
+const App = ({initialButtonText, initialButtonClass}) => {
+
+// const App = (props) => {
     /**
      * Hook useState
      * 1. destructuring object
      * initial value is buttonText = 'Click me' (buttonText = 'Click me')
      * !!! setButtonText - it is function -> we must call a function
      */
-    const [buttonText, setButtonText] = React.useState('Click me');
-    const [buttonClass, setButtonClass] = React.useState('default-btn');
+    //const {initialButtonText} = props;
+    const [buttonText, setButtonText] = React.useState(initialButtonText);
+    const [buttonClass, setButtonClass] = React.useState(initialButtonClass);
     //Function which call when we click button
     // Use Template literals, back quotes
     const onButtonClick = ()=> {
@@ -29,4 +36,7 @@ const App = () => {
 //Render component on page
 const containerJSX = document.getElementById('appJsx');
 const  rootJsx = ReactDOM.createRoot(containerJSX);
-rootJsx.render(<App />);
+rootJsx.render(<App initialButtonText= "Click me please" 
+                    initialButtonClass= "default-btn" 
+                />
+);
