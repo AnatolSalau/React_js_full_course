2e1f75f8287829d4696d736c9fa8f56a84172f69
AnatolSalau <salovanatoly5@mail.ru>
2023-02-11 21:05:54 +0300

    lesson_11_state_via_props

--- lesson_11_state_via_props/src/App.js
+++ lesson_11_state_via_props/src/App.js
@@ -1,11 +1,25 @@
+import {useState} from "react";
+
 import './App.css';
+import Button from "./components/Button";
+import Counter from "./components/Counter";
 
 function App() {
-  return (
+    console.log('App render')
+    const [count, setCount] = useState(0);
+     const incrementCount = () => {
+       setCount(count + 1);
+         console.log(count)
+    }
+    return (
     <div className="App">
-
+        <Counter count={count}/>
+        <Button onClick={incrementCount} />
+        <Button onClick={incrementCount} />
+        <Button onClick={incrementCount} />
+        <Button onClick={incrementCount} />
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ lesson_11_state_via_props/src/components/Button.js
@@ -0,0 +1,10 @@
+function Button({onClick}) {
+    console.log('Button render')
+    return (
+        <button onClick={onClick}>
+            Click me!
+        </button>
+    )
+}
+
+export default Button;
