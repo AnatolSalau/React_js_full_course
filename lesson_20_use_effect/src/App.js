cd862eea6c336d55b5a39bca673f242179823b9c
AnatolSalau <salovanatoly5@mail.ru>
2023-02-13 20:35:11 +0300

    lesson_20_use_effect

--- lesson_20_use_effect/src/App.js
+++ lesson_20_use_effect/src/App.js
@@ -1,6 +1,20 @@
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  console.log('App render')
+  const [todo, setTodo] =  useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => {
+          setTodo(json);
+        });
+    }, []
+  );
+  console.log(todo)
+
   return (
     <div className="App">
 
