76bcd8c50630abefa7a4f1c7ff509e4513ba0b17
AnatolSalau <salovanatoly5@mail.ru>
2023-02-13 12:08:00 +0300

    lesson_16_react_fragment initial commit

---
+++ lesson_16_react_fragment/src/components/Card.js
@@ -0,0 +1,12 @@
+//Use React fragment
+const Card = () => {
+  return (
+      <>
+          <h1>Anatoly</h1>
+          <h3>I am recording this course</h3>
+          <button>Like!</button>
+      </>
+  );
+}
+
+export default Card;
