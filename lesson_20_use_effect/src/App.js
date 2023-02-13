aa9840ad23b7475af673956e29238ba7d19d43d8
AnatolSalau <salovanatoly5@mail.ru>
2023-02-13 21:37:26 +0300

    lesson_20_use_effect

--- lesson_20_use_effect/src/App.js
+++ lesson_20_use_effect/src/App.js
@@ -5,19 +5,31 @@ function App() {
   console.log('App render')
   const [todo, setTodo] =  useState(null);
 
+  const [click, setClick] = useState(0);
+
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
         .then(response => response.json())
         .then(json => {
           setTodo(json);
+          console.log('Fetch is called')
         });
-    }, []
+    }, [click]
   );
   console.log(todo)
 
   return (
     <div className="App">
-
+        {todo && (
+          <h1>
+              {todo.title}
+          </h1>
+        )}
+        <button onClick= {() => setClick(prevState =>
+            prevState + 1
+        )}>
+            Get request from server
+        </button>
     </div>
   );
 }
