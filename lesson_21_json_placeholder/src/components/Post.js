77cf6913759b80226afd961ac839974ec35d4276
AnatolSalau <salovanatoly5@mail.ru>
2023-02-14 12:01:01 +0300

    lesson_21_json_placeholder

--- lesson_21_json_placeholder/src/components/Post.js
+++ lesson_21_json_placeholder/src/components/Post.js
@@ -1,4 +1,21 @@
+import {useEffect, useState} from "react";
+
 function Post() {
+    console.log('App render')
+    //Data from server
+    const [data, setData] = useState(null);
+
+    //Request to server by useEffect
+    useEffect(()=> {
+            fetch('https://jsonplaceholder.typicode.com/posts')
+                .then(response => response.json())
+                .then(json => {
+                    setData(json);
+                })
+        }, []
+    );
+    console.log(data)
+
     return (
         <h1>
             This is post
