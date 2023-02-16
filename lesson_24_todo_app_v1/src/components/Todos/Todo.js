e3e91efb74ebe9018766a58cd90471fb388947c2
AnatolSalau <salovanatoly5@mail.ru>
2023-02-16 23:22:13 +0300

    lesson_24_todo_app_v1

--- lesson_24_todo_app_v1/src/components/Todos/Todo.js
+++ lesson_24_todo_app_v1/src/components/Todos/Todo.js
@@ -1,12 +1,16 @@
-import { RiTodoFill } from 'react-icons/ri'
+import { RiTodoFill } from "react-icons/ri";
+
 import style from './Todo.module.css'
 
 function Todo({todo, deleteTodo}) {
+
     return (
+
         <div
             className={style.todo}
             onDoubleClick={() => deleteTodo(todo.id)}
         >
+            <RiTodoFill className={style.todoIcon}/>
             {`ID : ${todo.id}, text : ${todo.text}`}
         </div>
     )
