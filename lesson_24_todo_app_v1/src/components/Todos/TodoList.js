d36b0d2016a258deba383114f06145112c098606
AnatolSalau <salovanatoly5@mail.ru>
2023-02-16 15:59:30 +0300

    lesson_24_todo_app_v1 CSS TodoForm add

--- lesson_24_todo_app_v1/src/components/Todos/TodoList.js
+++ lesson_24_todo_app_v1/src/components/Todos/TodoList.js
@@ -1,13 +1,20 @@
 import Todo from "./Todo";
+import style from './TodoList.module.css'
 
 function TodoList({todoList}) {
-    return todoList.map(
+    return (
+        <div className={style.todoList}>
+            {
+                todoList.map(
                     (todo, index) => {
                         return (
                             <Todo key={index} todo={todo} />
                         )
                     }
                 )
+            }
+        </div>
+    )
 }
 
 export default TodoList;
