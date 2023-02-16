6dff6550144ffabb74baefc4fe855aab1910f5b1
AnatolSalau <salovanatoly5@mail.ru>
2023-02-16 10:53:54 +0300

    lesson_24_todo_app_v1

--- lesson_24_todo_app_v1/src/components/Todos/TodoForm.js
+++ lesson_24_todo_app_v1/src/components/Todos/TodoForm.js
@@ -1,9 +1,41 @@
+import style from './TodoForm.module.css'
+import {useState} from "react";
+
 function TodoForm() {
+
+    //Initialize data in inputs by object
+    const [data, setData] = useState(
+        {todo: 'initial todo'}
+    );
+
+    //Handle input when press submit
+    const handleInputSubmit = (event) => {
+        //Stop reload page when we press submit
+        event.preventDefault();
+        //Logic
+        console.log("Submit was pressed, data : " + data.todo)
+    }
+
+    //Handle changing in input dynamically
+    const handleInputChange = (event, nameInput) => {
+      setData(
+          {...data, [nameInput]: event.target.value}
+      )
+    }
+
     return (
-        <div>
-            <input type="text"/>
+        <form className={style.todoForm} onSubmit={handleInputSubmit}>
+            <input
+                type="text"
+                value={data.todo}
+                onChange={
+                    (event) => {
+                        handleInputChange(event, 'todo');
+                    }
+                }
+            />
             <button type="submit">Submit</button>
-        </div>
+        </form>
     )
 }
 
