cc04f9f42c7900d7bbeb8805974c78ca05d39be1
AnatolSalau <salovanatoly5@mail.ru>
2023-02-16 16:18:08 +0300

    lesson_24_todo_app_v1 CSS TodoForm add

--- lesson_24_todo_app_v1/src/components/Todos/TodoList.js
+++ lesson_24_todo_app_v1/src/components/Todos/TodoList.js
@@ -5,13 +5,15 @@ function TodoList({todoList}) {
     return (
         <div className={style.todoList}>
             {
-                todoList.map(
+                todoList.length === '0'
+                ? (todoList.map(
                     (todo, index) => {
                         return (
                             <Todo key={index} todo={todo} />
                         )
-                    }
+                    })
                 )
+                : <h1>Doctor tasks is empty</h1>
             }
         </div>
     )
