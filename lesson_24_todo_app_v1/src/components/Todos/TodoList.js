608eda843174c77a13f951dab99f2340932e43b0
AnatolSalau <salovanatoly5@mail.ru>
2023-02-16 15:00:22 +0300

    lesson_24_todo_app_v1 My own solution

--- lesson_24_todo_app_v1/src/components/Todos/TodoList.js
+++ lesson_24_todo_app_v1/src/components/Todos/TodoList.js
@@ -1,19 +1,13 @@
 import Todo from "./Todo";
 
 function TodoList({todoList}) {
-    return (
-        <>
-            {
-                todoList.map(
+    return todoList.map(
                     (todo, index) => {
                         return (
                             <Todo key={index} todo={todo} />
                         )
                     }
                 )
-            }
-        </>
-    )
 }
 
 export default TodoList;
