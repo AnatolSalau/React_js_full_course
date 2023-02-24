db8ffd4a1cadc3824ef015cd755dfcc94ded92e8
AnatolSalau <salovanatoly5@mail.ru>
2023-02-24 16:16:39 +0300

    lesson_25_react_router initial router relative path

--- lesson_25_react_router/src/App.js
+++ lesson_25_react_router/src/App.js
@@ -1,20 +1,22 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-
-import './App.css';
 import Home from "./components/Home";
 import Contacts from "./components/Contacts";
 import About from "./components/About";
 import NotFound from "./components/NotFound";
+import MainLayout from "./layouts/MainLayout";
+import './App.css';
 
 function App() {
       return (
             <BrowserRouter>
                   <div className="App">
                         <Routes>
-                              <Route path="/" element={<Home />}/>
-                              <Route path="about" element={<About />}/>
-                              <Route path="contacts" element={<Contacts />}/>
-                              <Route path="*" element={<NotFound />}/>
+                              <Route path="/main" element={<MainLayout/>}>
+                                    <Route index element={<Home/>}/>
+                                    <Route path="about" element={<About/>}/>
+                                    <Route path="contacts" element={<Contacts/>}/>
+                                    <Route path="*" element={<NotFound/>}/>
+                              </Route>
                         </Routes>
                   </div>
             </BrowserRouter>
