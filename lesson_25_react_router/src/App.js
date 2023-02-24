d8d88fece9f6c2326890bee5c258adccf20e8853
AnatolSalau <salovanatoly5@mail.ru>
2023-02-24 12:06:10 +0300

    lesson_25_react_router initial router path

--- lesson_25_react_router/src/App.js
+++ lesson_25_react_router/src/App.js
@@ -1,18 +1,24 @@
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import './App.css';
 
-
 function App() {
-        return (
-                <BrowserRouter>
-                        <div className="App">
-                                <h1>
-                                        React router
-                                </h1>
-                        </div>
-                </BrowserRouter>
-        );
+      return (
+            <BrowserRouter>
+                  <div className="App">
+                        <Routes>
+                              <Route
+                                    path="/"
+                                    element={
+                                    <h1>
+                                          Home page
+                                    </h1>
+                              }>
+                              </Route>
+                        </Routes>
+                  </div>
+            </BrowserRouter>
+      );
 }
 
 export default App;
