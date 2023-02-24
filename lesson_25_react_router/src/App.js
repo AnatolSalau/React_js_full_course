8da0d5104cb20240b08c2af5567a0504d74b36bf
AnatolSalau <salovanatoly5@mail.ru>
2023-02-24 11:12:00 +0300

    lesson_25_react_router initial commit

--- lesson_25_react_router/src/App.js
+++ lesson_25_react_router/src/App.js
@@ -1,11 +1,15 @@
+import {BrowserRouter} from 'react-router-dom';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+        return (
+                <BrowserRouter>
+                        <div className="App">
 
-    </div>
-  );
+                        </div>
+                </BrowserRouter>
+        );
 }
 
 export default App;
