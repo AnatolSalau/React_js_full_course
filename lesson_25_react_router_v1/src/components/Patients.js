171172adb23d0803b64deab6fe7157c9a6df4457
AnatolSalau <salovanatoly5@mail.ru>
2023-02-27 21:13:27 +0300

    lesson_26_npm_query_string sorted courses

--- lesson_25_react_router_v1/src/components/Patients.js
+++ lesson_25_react_router_v1/src/components/Patients.js
@@ -1,22 +1,35 @@
 import {useLocation} from "react-router-dom";
 import queryString from 'query-string';
+import {useState} from "react";
 
-import patientsData from '../data/patients.json'
+import coursesData from '../data/patients.json'
 
 import PatientCard from "./PatientCard";
 
+function sortCourses(courses, key) {
+      const sortedCourses = [...courses];
+      sortedCourses.sort((a, b) => {
+            return a[key] > b[key] ? 1 : -1;
+      });
+      return sortedCourses;
+}
 
 function Patients() {
 
       const location = useLocation();
       const query = queryString.parse(location.search);
-      console.log(query);
-      
+
+      let [sortKey, setSortKey] = useState(query.sort);
+
+      let [sortedCourses, setSortedCourses] = useState(
+            sortCourses(coursesData, sortKey)
+      );
+
       return (
             <>
                   <h1>Все пациенты:</h1>
                   {
-                        patientsData.map((patient => {
+                        sortedCourses.map((patient => {
                               return <PatientCard key={patient.id}  {...patient}/>
                         }))
                   }
@@ -24,4 +37,4 @@ function Patients() {
       );
 }
 
-export default Patients
+export default Patients;
